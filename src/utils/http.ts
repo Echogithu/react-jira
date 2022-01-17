33f8a2cd973ee66a878137c9054b4e6a5aa783d7
Zeping.Cai <zeping.cai@raysightmed.com>
2022-01-17 17:47:35 +0800

    styles: ts别名学习

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -47,9 +47,55 @@ export const http = async (
     });
 };
 
+// JS 中的typeof 是在runtime时运行的
+// return typeof 1 === 'number'
+
+// TS 中的typeof 是在静态环境运行的
+// return (...[endpoint, config]: Parameters<typeof http>) =>
+
 export const useHttp = () => {
   const { user } = useAuth();
 
+  // utility type的用法： 用泛型给它传入一个其他类型， 然后utility type 对这个类型进行某种操作
   return (...[endpoint, config]: Parameters<typeof http>) =>
     http(endpoint, { ...config, token: user?.token });
 };
+
+// 类型别名在很多情况下可以和interface互换
+// interface Person {
+//   name: string;
+// }
+// type Person = {
+//   name: string;
+// };
+// const xiaoming: Person = { name: "xiaoming" };
+
+// 类型别名， interface在这种情况(联合类型或者交叉类型)下没法替代 type
+// type FavoriteNumber = string | number;
+// let roseFavoriteNumber: FavoriteNumber = '7'
+
+// interface 也没法实现Utility type
+// type Person = {
+//   name: string;
+//   age: number;
+// };
+// Partial是让属性都变为可选
+// const xiaoming: Partial<Person> = { name: "小明" };
+// Omit是删除某些属性，必须传入其余属性
+// const shenmiren: Omit<Person, "name"> = { age: 8 };
+// const shenmiren: Omit<Person, "name" | "age"> = { age: 8 };
+
+// type PersonKeys = keyof Person // 从Person中取出key
+// Partial的实现
+// type Partial<T> = {
+//   [P in keyof T]?: T[P];
+// };
+
+// type PersonOnlyName = Pick<Person, 'name'>
+// type Age = Exclude<PersonKeys, 'name'>
+// Omit的实现
+// type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+// type Pick<T, K extends keyof T> = {
+//   [P in K]: T[P];
+// };
+// type Exclude<T, U> = T extends U ? never : T;
